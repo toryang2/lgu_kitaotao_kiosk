2fbc228cca49cecdf0a83f31a1389532746fa9a3
toryang2 <upszboh2@gmail.com>
2024-09-12 16:02:33 +0800

    !update

---
+++ app/src/main/java/com/kitaotao/sst/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.kitaotao.sst
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kitaotao.sst.office.MunicipalAccountingOffice
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+        val macco_txt_button  = findViewById<TextView>(R.id.macco)
+        macco_txt_button.setOnClickListener {
+            val intent = Intent(this, MunicipalAccountingOffice::class.java)
+            startActivity(intent)
+        }
+    }
+}
