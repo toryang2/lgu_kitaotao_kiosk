5b7e9d3e42477f812b73796f12de458258b86367
toryang2 <upszboh2@gmail.com>
2024-09-26 14:51:15 +0800

    !update

--- app/src/main/java/com/kitaotao/sst/office/MunicipalAccountingOffice.kt
+++ app/src/main/java/com/kitaotao/sst/office/MunicipalAccountingOffice.kt
@@ -1,11 +1,17 @@
 package com.kitaotao.sst.office
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.kitaotao.sst.R
+import com.kitaotao.sst.services.accounting.accounting_service_1
+import com.kitaotao.sst.services.accounting.accounting_service_2
+import com.kitaotao.sst.services.accounting.accounting_service_3
+
 
 class MunicipalAccountingOffice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +22,21 @@ class MunicipalAccountingOffice : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val accountingService1 = findViewById<TextView>(R.id.service_1)
+        accountingService1.setOnClickListener{
+            val intent = Intent(this, accounting_service_1::class.java)
+            startActivity(intent)
+        }
+        val accountingService2 = findViewById<TextView>(R.id.service_2)
+        accountingService2.setOnClickListener{
+            val intent = Intent(this, accounting_service_2::class.java)
+            startActivity(intent)
+        }
+        val accountingService3 = findViewById<TextView>(R.id.service_3)
+        accountingService3.setOnClickListener{
+            val intent = Intent(this, accounting_service_3::class.java)
+            startActivity(intent)
         }
     }
 }
