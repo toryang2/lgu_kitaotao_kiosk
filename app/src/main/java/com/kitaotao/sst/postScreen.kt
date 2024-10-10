fbf0fb1e16865aa360b1df25b4ac3fba9822307f
Toryang2 <upszboh2@gmail.com>
2024-10-10 18:52:16 +0800

    added postScreen before mainview

---
+++ app/src/main/java/com/kitaotao/sst/postScreen.kt
@@ -0,0 +1,39 @@
+package com.kitaotao.sst
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+class postScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().setKeepOnScreenCondition {
+            runBlocking {
+                delay(3000L)
+                false
+            }
+        }
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_post_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setClickListener(R.id.imageButton, MainActivity::class.java)
+    }
+
+    private fun setClickListener(viewId: Int, activityClass: Class<*>) {
+        findViewById<ImageButton>(viewId).setOnClickListener {
+            startActivity(Intent(this, activityClass))
+        }
+    }
+}
