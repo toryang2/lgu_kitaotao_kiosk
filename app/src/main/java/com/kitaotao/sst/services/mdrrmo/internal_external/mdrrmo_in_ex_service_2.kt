19a1caed42522560baafad89986d738f4618399d
toryang2 <upszboh2@gmail.com>
2024-10-17 16:21:09 +0800

    update done all mdrrmo

---
+++ app/src/main/java/com/kitaotao/sst/services/mdrrmo/internal_external/mdrrmo_in_ex_service_2.kt
@@ -0,0 +1,44 @@
+package com.kitaotao.sst.services.mdrrmo.internal_external
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kitaotao.sst.MainActivity
+import com.kitaotao.sst.R
+
+class mdrrmo_in_ex_service_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.mdrrmo_in_ex_service2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        //Back Button
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                finish()
+            }
+        })
+
+        val backButton: Button = findViewById(R.id.buttonBack)
+        backButton.setOnClickListener {
+            finish()
+        }
+        //end of back button
+        val homeButton: Button = findViewById(R.id.buttonHome)
+        homeButton.setOnClickListener {
+            // Start MainActivity directly without delay
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: Finish this activity to remove it from the back stack
+        }
+    }
+}
