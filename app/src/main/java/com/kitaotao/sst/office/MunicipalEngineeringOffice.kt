dae8787af4d2462fa1aea62bcabd4caf7941ee0a
toryang2 <upszboh2@gmail.com>
2024-10-18 16:09:53 +0800

    update done all engineering

---
+++ app/src/main/java/com/kitaotao/sst/office/MunicipalEngineeringOffice.kt
@@ -0,0 +1,51 @@
+package com.kitaotao.sst.office
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kitaotao.sst.MainActivity
+import com.kitaotao.sst.R
+import com.kitaotao.sst.services.engineering.*
+
+class MunicipalEngineeringOffice : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.office_municipal_engineering_office)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val backButton: Button = findViewById(R.id.buttonBack)
+        backButton.setOnClickListener {
+            finish() // Finish current activity when back button is clicked
+        }
+
+        // Home Button implementation
+        val homeButton: Button = findViewById(R.id.buttonHome)
+        homeButton.setOnClickListener {
+            // Start MainActivity directly without delay
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: Finish this activity to remove it from the back stack
+        }
+
+        // Set click listeners for various services
+        setClickListener(R.id.buttonBack, MainActivity::class.java)
+        setClickListener(R.id.service_1, engineering_service_1::class.java)
+        setClickListener(R.id.service_2, engineering_service_2::class.java)
+
+    }
+    private fun setClickListener(viewId: Int, activityClass: Class<*>){
+        findViewById<TextView>(viewId).setOnClickListener{
+            startActivity(Intent(this, activityClass))
+        }
+    }
+}
