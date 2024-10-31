3730719cf2399462a89aea80808522fd48a4d42d
toryang2 <upszboh2@gmail.com>
2024-10-31 09:43:28 +0800

    add liga and ledipo, finished all liga and ledipo services

---
+++ app/src/main/java/com/kitaotao/sst/services/ledipo/external/ledipo_ex_service_1.kt
@@ -0,0 +1,21 @@
+package com.kitaotao.sst.services.ledipo.external
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.kitaotao.sst.R
+
+class ledipo_ex_service_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.ledipo_ex_service1)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
