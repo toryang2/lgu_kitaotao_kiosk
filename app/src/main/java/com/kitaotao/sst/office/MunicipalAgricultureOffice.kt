f69cd707f81b6a310a26a80bbc9abf9be46ab509
toryang2 <upszboh2@gmail.com>
2024-12-10 14:56:28 +0800

    Fix minor bugs, added map to the offices on some office.

--- app/src/main/java/com/kitaotao/sst/office/MunicipalAgricultureOffice.kt
+++ app/src/main/java/com/kitaotao/sst/office/MunicipalAgricultureOffice.kt
@@ -2,6 +2,7 @@ package com.kitaotao.sst.office
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -44,7 +45,13 @@ class MunicipalAgricultureOffice : AppCompatActivity() {
         setClickListener(R.id.service_11, agriculture_service_11::class.java)
         setClickListener(R.id.service_12, agriculture_service_12::class.java)
         setClickListener(R.id.service_13, agriculture_service_13::class.java)
+
+        val imageView = findViewById<ImageView>(R.id.imageLayout)
+
+// Set the drawable resource for the ImageView
+        imageView.setImageResource(R.drawable.kitaotao_1st_floor_model)
     }
+
     private fun setClickListener(viewId: Int, activityClass: Class<*>){
         findViewById<TextView>(viewId).setOnClickListener{
             startActivity(Intent(this, activityClass))
--- app/src/main/java/com/kitaotao/sst/office/MunicipalCivilRegistryOffice.kt
+++ app/src/main/java/com/kitaotao/sst/office/MunicipalCivilRegistryOffice.kt
@@ -2,6 +2,7 @@ package com.kitaotao.sst.office
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -45,7 +46,12 @@ class MunicipalCivilRegistryOffice : AppCompatActivity() {
         setClickListener(R.id.service_12, civilRegistry_service_12::class.java)
         setClickListener(R.id.service_13, civilRegistry_service_13::class.java)
 
+        val imageView = findViewById<ImageView>(R.id.imageLayout)
+
+// Set the drawable resource for the ImageView
+        imageView.setImageResource(R.drawable.kitaotao_1st_floor_model)
     }
+
     private fun setClickListener(viewId: Int, activityClass: Class<*>){
         findViewById<TextView>(viewId).setOnClickListener{
             startActivity(Intent(this, activityClass))
